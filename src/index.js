99f080412d84fe487d633765e05ece44e80762af
Balaji A <93375333+Balaji21121109@users.noreply.github.com>
2022-05-07 21:18:41 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import './index.css'
+
+
+
+ReactDOM.render(
+<div id="main">
+    <h1>This is the main website of react</h1>
+    <div id="sec">
+        <div className="abc"></div>
+        <div className="abc"></div>
+        <div className="abc"></div>
+        <div className="abc"></div>
+
+    </div>
+</div>,document.getElementById("root")
+);
